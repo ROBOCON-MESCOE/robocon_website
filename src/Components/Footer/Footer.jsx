ef0217d5cf0b3e55849ba60afd0cce4c25fbff39
tanmayjagtap <tanmayjagtap27@gmail.com>
2021-06-12 11:06:30 +0530

    added our events

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return(
         <div className="bg-black white tc f4 pa3">
-            Developed by <a className="b pointer white" href="https://debuggedexe.com/#/">debugged.exe</a>❤️
+            Developed by <a className="b pointer white" href="https://debuggedexe.com/#/">debugged.exe</a>🐞
         </div>
     );
 }
--- src/Components/OurEvents/OurEvents.jsx
+++ src/Components/OurEvents/OurEvents.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import './OurEvents.css';
 
 const OurEvents = () => {
     return (
-        <div>
-            Hello
+        <div className="flex justify-around">
+            <div className="w-50 flex justify-center items-center">
+                <img style={{height:"360px"}} src="https://images.unsplash.com/photo-1485827404703-89b55fcc595e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt=" "></img>
+            </div>
+            <div className="w-50 flex flex-column justify-center items-center">
+                <h1>Heading</h1>
+                <p className="ph4">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+                <a class="f6 link shadow-5 ph3 pv2 white bg-dark-blue grow" href="#0">Button Text</a>
+            </div>
         </div>
     );
 }
