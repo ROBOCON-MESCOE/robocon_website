4b9077b2eb2154d82832a128181e0f63c23b1d15
sayali-s <shindesayali85@gmail.com>
2021-06-15 00:06:41 +0530

    Gallery transitions.

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -5,14 +5,63 @@ const Gallery = () => {
     return (
         <div className="mb4">
             <div className="grid-container">
-                <div className="grid-item grid-item-1"> </div>
-                <div className="grid-item grid-item-2"> </div>
-                <div className="grid-item grid-item-3"> </div>
-                <div className="grid-item grid-item-4"> </div>
-                <div className="grid-item grid-item-5"> </div>
-                <div className="grid-item grid-item-6"> </div>
-                <div className="grid-item grid-item-7"> </div>
-                <div className="grid-item grid-item-8"> </div>
+
+                <div className="grid-item grid-item-1 flex flex-column justify-center items-center white">
+                    <div className="item-1-text">
+                        <h1> Event 1 </h1>
+                        <h3>June 20, 2020</h3>
+                    </div>
+                </div>
+
+                <div className="grid-item grid-item-2 flex flex-column justify-center items-center white">
+                    <div className="item-2-text">
+                        <h1> Event 1 </h1>
+                        <h3>June 20, 2020</h3>
+                    </div>
+                </div>
+
+                <div className="grid-item grid-item-3 flex flex-column justify-center items-center white">
+                    <div className="item-3-text">
+                        <h1> Event 1 </h1>
+                        <h3>June 20, 2020</h3>
+                    </div>
+                </div>
+
+                <div className="grid-item grid-item-4 flex flex-column justify-center items-center white">
+                    <div className="item-4-text">
+                        <h1> Event 1 </h1>
+                        <h3>June 20, 2020</h3>
+                    </div>
+                </div>
+
+                <div className="grid-item grid-item-5 flex flex-column justify-center items-center white">
+                    <div className="item-5-text">
+                        <h1> Event 1 </h1>
+                        <h3>June 20, 2020</h3>
+                    </div>
+                </div>
+
+                <div className="grid-item grid-item-6 flex flex-column justify-center items-center white">
+                    <div className="item-6-text">
+                        <h1> Event 1 </h1>
+                        <h3>June 20, 2020</h3>
+                    </div>
+                </div>
+
+                <div className="grid-item grid-item-7 flex flex-column justify-center items-center white">
+                    <div className="item-7-text">
+                        <h1> Event 1 </h1>
+                        <h3>June 20, 2020</h3>
+                    </div>
+                </div>
+
+                <div className="grid-item grid-item-8 flex flex-column justify-center items-center white">
+                    <div className="item-8-text">
+                        <h1> Event 1 </h1>
+                        <h3>June 20, 2020</h3>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
